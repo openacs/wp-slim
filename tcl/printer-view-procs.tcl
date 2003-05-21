0c3f47d6b2c65be02ad165f10d5d8988191daff2
Rocael Hernández Rizzardini <roc@viaro.net>
2003-05-21 14:37:34 +0000

    changes for new wp

--- tcl/printer-view-procs.tcl
+++ tcl/printer-view-procs.tcl
@@ -7,16 +7,6 @@ ad_proc get_attach_list {
 slide_item_id
 }  {
 
-db_multirow attach_list get_attachments {
-    
-    select live_revision as attach_id, display, name as file_name
-    from (select live_revision, name
-          from cr_items
-          where parent_id = :slide_item_id and
-                content_type in ('cr_wp_image_attachment', 'cr_wp_file_attachment')
-          ) a
-    left join cr_wp_image_attachments on (a.live_revision = attach_id)
-
-}
+db_multirow attach_list get_attachments { *SQL* }
 
 }
