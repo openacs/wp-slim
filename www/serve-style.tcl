c6c80ad1c65908534a0f94b392a9f4a30698527d
Rocael Hernández Rizzardini <roc@viaro.net>
2003-05-21 15:41:50 +0000

    scalability revision, permissions, bug fixing, enhancements

---
+++ www/serve-style.tcl
@@ -0,0 +1,29 @@
+# /packages/wp-slim/www/serve-style.tcl
+ad_page_contract {
+     
+    # This file builds the respective CSS for a specific defined style.
+     @author Rocael HR (roc@viaro.net)
+} {
+} 
+
+set url [ad_conn url]
+
+if {![regexp {styles/(default|[0-9]+)/(.*)} $url match style_id file_name]} {
+    ns_log notice "Could not get a style_id out of url=$url"
+    ad_abort_script
+}
+
+if { $style_id == "default" } {
+       set style_id -1
+}
+
+
+# Serve the top presentation page.
+set css [db_string get_style {
+    select css
+    from wp_styles
+    where style_id = :style_id
+}]
+
+
+doc_return  200 "text/css" $css
