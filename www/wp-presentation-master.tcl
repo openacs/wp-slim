e02ff22606305dcbc99d571788289338b5811f9e
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-04-12 16:12:57 +0000

    - added victor's patch to omit deprecated messages of "ns_info tcllib". Background: naviserver supports multiple (virtual) servers, which might have different tcllibs. Therefore, the preferred command to query this was moved to "ns_server ?-server s? tcllib" and "ns_info tcllib" was deprecated. From the OpenACS point of view, one wants to locate the actual library. Since the variable does not change, it is fine to store it in a per-thread variable which can be efficiently accessed.

--- www/wp-presentation-master.tcl
+++ www/wp-presentation-master.tcl
@@ -1,4 +1,4 @@
-set root_directory [file dirname [string trimright [ns_info tcllib] "/"]]
+set root_directory [file dirname [string trimright $::acs::tcllib "/"]]
 source "$root_directory/www/default-master.tcl"
 set package_url [ad_conn package_url]
 set wp_header [wp_header $style_id]
